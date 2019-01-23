ab1247235b9277d66dd2e4e6b69d36ea057ac299
maaax128 <44034742+maaax128@users.noreply.github.com>
2019-01-23 20:54:14 +0500

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $userId=$_SESSION['user_id'];
-$taskId=$_POST['task_id'];
+$taskId=$_SESSION['id'];
 require('connect.php');
 $taskDelete = $pdo->prepare("DELETE FROM task WHERE user_id='$userId' AND id='$taskId' LIMIT 1"); 
 $taskDelete->execute();
