1a848daabf7baa39bed28ce1fa1cef3fcf4cd172
maaax128 <44034742+maaax128@users.noreply.github.com>
2019-01-23 20:55:02 +0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,3 +1,6 @@
 <?php
+<?php
+//$pdo = new PDO("mysql:host=localhost;charset=UTF8; dbname=hw", "root","985958");
 $pdo = new PDO("mysql:host=localhost;charset=UTF8; dbname=mmaltsev", "mmaltsev","neto1891");
+?>
 ?>
