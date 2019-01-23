c9956e901de566d666437976029cdd25dbcdbae1
maaax128 <44034742+maaax128@users.noreply.github.com>
2019-01-23 20:52:51 +0500

    Update transfer.php

--- transfer.php
+++ transfer.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $userId=$_SESSION['user_id'];
-$taskId=$_POST['task_id'];
+$taskId=$_SESSION['id'];
 $assignedUserId = $_POST['assigned_user_id'];
 require('connect.php');
 $assignedUserId = $pdo->prepare("UPDATE task SET assigned_user_id='$assignedUserId' WHERE user_id='$userId' AND id='$taskId' LIMIT 1");
