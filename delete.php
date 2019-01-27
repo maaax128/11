7ccb010a122559e88b4808da567e3b449689112b
maaax128 <44034742+maaax128@users.noreply.github.com>
2019-01-27 19:21:58 +0500

    Delete delete.php

--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$userId=$_SESSION['user_id'];
-$taskId=$_SESSION['id'];
-require('connect.php');
-$taskDelete = $pdo->prepare("DELETE FROM task WHERE user_id='$userId' AND id='$taskId' LIMIT 1"); 
-$taskDelete->execute();
-header("Location:toDoList.php");
-?>
