839dbc5c5e64371308d73bbc6ba05ec4720b878e
maaax128 <44034742+maaax128@users.noreply.github.com>
2019-01-27 19:21:52 +0500

    Delete connect.php

--- connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-<?php
-//$pdo = new PDO("mysql:host=localhost;charset=UTF8; dbname=hw", "root","985958");
-$pdo = new PDO("mysql:host=localhost;charset=UTF8; dbname=mmaltsev", "mmaltsev","neto1891");
-?>
-?>
